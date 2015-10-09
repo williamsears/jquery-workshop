86b8d70e807181adcd930257302fdecc78f5eace
Ziad Saab <ziad.saab@gmail.com>
2015-10-09 15:37:27 +0000

    exercise 3

--- script/app.js
+++ script/app.js
@@ -1,1 +1,19 @@
 // Write your code here!
+$('#signup-form').on('submit', function(event) {
+  var password1 = $('input[name=password]').val();
+  var password2 = $('input[name=password2]').val();
+  
+  if (password1 !== password2) {
+      alert('passwords dont match')
+  }
+  else if (password1.length < 10) {
+      alert('password is too short');
+  }
+  else {
+      return;
+  }
+  
+  event.preventDefault();
+  
+  
+});
