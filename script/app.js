0f5b09b2471a01b216b6df3c250d88fadd6652c0
Ziad Saab <ziad.saab@gmail.com>
2015-10-09 15:57:31 +0000

    exerise 4

--- script/app.js
+++ script/app.js
@@ -1,1 +1,13 @@
 // Write your code here!
+$.getJSON('http://api.open-notify.org/iss-now.json?callback=?').then(
+    function(res) {
+        var lat = res.iss_position.latitude;
+        var lng = res.iss_position.longitude;
+        
+        // setTimeout here is just for show...
+        setTimeout(function() {
+            $('#iss-position').text('the ISS is at ' + lat + ', ' + lng);
+        }, 2000);
+        
+    }
+);
