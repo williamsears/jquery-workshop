85a7f6e505e317dc0db4669f6e92ddfd1e309b02
William Sears <wpeter1@hotmail.com>
2015-12-10 19:59:15 +0000

    exercise 1

--- script/app.js
+++ script/app.js
@@ -1,1 +1,19 @@
 // Write your code here!
+$(document).ready(function(){
+    var $butn1= $('<input/>').attr({ onclick: "change()", type: 'button', name:'btn1', value:'X', id: "id", class: "ok"});
+   $('.announcement').append($butn1);
+    
+    $('.ok').click(function(){
+     if (this.value === "X"){
+this.value="View the Content"
+    $(this).parent().children().hide();
+    $(this).show();
+    
+}
+else {
+    this.value="X";
+    $(this).siblings().show();
+}
+    })
+
+})
